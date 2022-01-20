da90fa8a261b175d0eee92095c7ff9cbc255ceae
Elías <solerurban.elias@gmail.com>
2022-01-20 20:12:08 +0100

    mejor

--- js/audio.js
+++ js/audio.js
@@ -1,19 +1,3 @@
-let reproducir1 = () => {
-    audio1.play();
-}
-
-let reproducir2 = () => {
-    audio2.play();
-}
-
-let reproducir3 = () => {
-    audio3.play();
-}
-
-let reproducir4 = () => {
-    audio4.play();
-}
-
 iniciar = () => {
     audio1 = document.getElementById("audio1");
     audio2 = document.getElementById("audio2");
@@ -25,10 +9,10 @@ iniciar = () => {
     boton3 = document.getElementById("boton3");
     boton4 = document.getElementById("boton4");
 
-    boton1.addEventListener("click", reproducir1);
-    boton2.addEventListener("click", reproducir2);
-    boton3.addEventListener("click", reproducir3);
-    boton4.addEventListener("click", reproducir4);
+    boton1.addEventListener("click", audio1.play());
+    boton2.addEventListener("click", audio2.play());
+    boton3.addEventListener("click", audio3.play());
+    boton4.addEventListener("click", audio4.play());
 }
 
 window.addEventListener("load", iniciar, false);
