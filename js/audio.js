cc5c3bd59d107676b55c7c33a7b69745ca2c92f5
Elías <solerurban.elias@gmail.com>
2022-01-20 20:09:11 +0100

    añadidos botones con audios

--- js/audio.js
+++ js/audio.js
@@ -1,35 +1,34 @@
-reproducir = () => {
-    //replantear cómo acceder al elemento que desencadena el evento
-    switch (boton.id) {
-        case "audio1":
-            alert("Primer botón");
-            break;
-        case "audio2":
-            alert("Segundo botón");
-            break;
-        case "audio3":
-            alert("Tercer botón");
-            break;
-        case "audio4":
-            alert("Cuarto botón");
-        default:
-            break;
-    }
+let reproducir1 = () => {
+    audio1.play();
+}
+
+let reproducir2 = () => {
+    audio2.play();
+}
+
+let reproducir3 = () => {
+    audio3.play();
+}
+
+let reproducir4 = () => {
+    audio4.play();
 }
 
 iniciar = () => {
-    boton1 = document.getElementById("audio1");
-    boton2 = document.getElementById("audio2");
-    boton3 = document.getElementById("audio3");
-    boton4 = document.getElementById("audio4");
+    audio1 = document.getElementById("audio1");
+    audio2 = document.getElementById("audio2");
+    audio3 = document.getElementById("audio3");
+    audio4 = document.getElementById("audio4");
 
-    botones = [boton1, boton2, boton3, boton4];
+    boton1 = document.getElementById("boton1");
+    boton2 = document.getElementById("boton2");
+    boton3 = document.getElementById("boton3");
+    boton4 = document.getElementById("boton4");
 
-    console.log(botones);
-
-    botones.forEach(boton => {
-        boton.addEventListener("click", reproducir);
-    });
+    boton1.addEventListener("click", reproducir1);
+    boton2.addEventListener("click", reproducir2);
+    boton3.addEventListener("click", reproducir3);
+    boton4.addEventListener("click", reproducir4);
 }
 
 window.addEventListener("load", iniciar, false);
