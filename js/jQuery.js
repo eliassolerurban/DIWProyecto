396679926daa02f1c285e715f20efd4879b78d75
Elías <solerurban.elias@gmail.com>
2022-02-16 18:54:50 +0100

    aside hecho

---
+++ js/jQuery.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $("#btn1").click(function() {
+        let position = $("#entrada1").offset();
+        $('html, body').animate({ scrollTop: position.top - 190 }, 'slow');
+    });
+    $("#btn2").click(function() {
+        let position = $("#entrada2").offset();
+        $('html, body').animate({ scrollTop: position.top - 190 }, 'slow');
+    });
+    $("#btn3").click(function() {
+        let position = $("#entrada3").offset();
+        $('html, body').animate({ scrollTop: position.top - 190 }, 'slow');
+    });
+    $("#btn4").click(function() {
+        let position = $("#entrada3").offset();
+        $('html, body').animate({ scrollTop: position.top - 190 }, 'slow');
+    });
+});
