99569da19f782787a37e764ad696eb2a7b8eb50f
Elías <solerurban.elias@gmail.com>
2022-02-17 11:17:02 +0100

    jquery hecho

--- js/jQuery.js
+++ js/jQuery.js
@@ -15,12 +15,28 @@ $(document).ready(function() {
         let position = $("#entrada3").offset();
         $('html, body').animate({ scrollTop: position.top - 190 }, 'slow');
     });
+
+    //no consigo que me aparezca la flecha
     $("#toTop").css("display", "none");
     $(window).scroll(function() {
         if ($(window).scrollTop() > 500) {
             $("#toTop").fadeIn("slow");
         } else {
             $("#toTop").fadeOut("slow");
+        }
+    });
+
+    let x = true;
+    $('#trigger').on("click", function(e) {
+        e.preventDefault();
+        if (x) {
+            $('.navegacionPrincipal').stop().slideToggle('fast');
+            x = false;
+        } else {
+            $('.navegacionPrincipal').stop().slideToggle('fast', function() {
+                $('.navegacionPrincipal').css('display', '');
+                x = true;
+            })
         }
     });
 });
